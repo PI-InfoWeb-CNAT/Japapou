3a1fd75989fedfd0670a9d1baba723a2cbf71fad
pdroluc4as <pedrolucasoliverdesousa@gmail.com>
2025-07-13 19:58:12 -0300

    imagens de comida adicionadas e um peixe

--- source/japapou/static/js/manager/manage_menu.js
+++ source/japapou/static/js/manager/manage_menu.js
@@ -39,9 +39,22 @@
 const addPratoExisten = document.getElementById('addpratoexistentbtn');
 const addNovoPrato = document.getElementById('addnovopratobtn');
 
+const modalNovoCardapio = document.getElementById('novo-cardapio')
 const modalNovoPrato = document.getElementById('novo-prato');
 const modalAddNovoPrato = document.getElementById('criar-prato');
 const modalPratosRegistrados = document.getElementById('pratos-registrados');
+
+modalNovoPrato.addEventListener("click", function(event) {
+    if (event.target === modalNovoPrato) {
+        modalNovoPrato.close();
+    }
+});
+
+modalNovoCardapio.addEventListener("click", function(event) {
+    if (event.target === modalNovoCardapio) {
+        modalNovoCardapio.close();
+    }
+});
 
 addPratoExisten.addEventListener('click', () => {
     modalNovoPrato.close();
