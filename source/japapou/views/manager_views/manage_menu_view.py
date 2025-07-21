4ef459bddc911e228fa552baac450994ea4f6be9
pdroluc4as <pedrolucasoliverdesousa@gmail.com>
2025-07-21 16:02:06 -0300

    começo da implementação

--- source/japapou/views/manager_views/manage_menu_view.py
+++ source/japapou/views/manager_views/manage_menu_view.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render  # type: ignore
-
+from japapou.models.prato import Prato
 
 def manager_menu_view(request):
-    return render(request, template_name="manager/manage_menu.html", status=200)
+    pratos = Prato.objects.all()
+    
+    context = {
+        "pratos": pratos
+    }
+
+    return render(request, template_name="manager/manage_menu.html", status=200, context=context)
